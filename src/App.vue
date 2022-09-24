1d4b158715bd51bf6707adfeb56f73532a7b0d3d
Gifaldy Azka <me@falcxxdev.ml>
2022-09-24 16:19:33 +0700

    fix: remove router function
    I only need & working with one page only

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,5 @@
 <template>
-    <nav>
-        <router-link :to="{ name: 'Home' }">Home</router-link> |
-        <router-link :to="{ name: 'About' }">About</router-link>
-    </nav>
-    <router-view></router-view>
+    <h1>af iy</h1>
 </template>
 
 <script lang="ts">
@@ -12,14 +8,4 @@ export default {
 };
 </script>
 
-<style lang="scss">
-nav {
-    text-align: center;
-    color: #fff;
-    a {
-        color: #41b883;
-        font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell,
-            'Open Sans', 'Helvetica Neue', sans-serif;
-    }
-}
-</style>
+<style lang="scss"></style>
